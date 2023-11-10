8b9fde6580c1d6807c8650081d57b815c7568ccc
Sujan Manandhar <sujan.manandhar@cotiviti.com>
2023-11-10 18:15:44 +0545

    Updates cypress test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,17 @@
 /// <reference types="cypress" />
 
 describe('automation test suite', () => {
-    it('login', ()=>{
+    beforeEach(() => {
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
         cy.get('[name="username"]').type("Admin")
         cy.get('[name="password"]').type("admin123")
-        cy.contains('Login').click()
+        cy.get('[type="submit"]').click()
+    })
+    it('Time test', ()=>{
+        cy.contains("Time").click();
+        cy.get(".oxd-topbar-header-breadcrumb").should("contain", "Time")
+        cy.get("[data-v-75e744cd]").children("input").type("Paul")
+        cy.get('[type="submit"]').click()
+    
     })
 })
