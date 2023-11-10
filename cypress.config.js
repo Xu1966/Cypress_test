f83418426ccd214c1b7e7a9d3332ff2d477a6b7f
Sujan Manandhar <sujan.manandhar@cotiviti.com>
2023-11-10 12:14:19 +0545

    Adds video in cypress and html report

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     baseUrl: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     excludeSpecPattern: ['**/1-getting-started','**/2-advanced-examples'],
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
